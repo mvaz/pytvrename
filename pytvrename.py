f68f23c7c43907dbbca85ab7936e6af9366ff6fd
Miguel Vaz <mvaz@dei.uminho.pt>
2009-09-03 22:25:41 +0200

    added a first test

--- pytvrename.py
+++ pytvrename.py
@@ -60,6 +60,32 @@ def scrapeFilename( filename ):
 def finalPath( baseDir, show, season, fileName):
 	return "%s/%s/Season %s/%s" % [ baseDir, show, season, fileName ]
 
+
+def levenshtein(s1, s2):
+	if len(s1) < len(s2):
+		return levenshtein(s2, s1)
+	if not s1:
+		return len(s2)
+	
+	previous_row = xrange(len(s2) + 1)
+	for i, c1 in enumerate(s1):
+		current_row = [i + 1]
+		for j, c2 in enumerate(s2):
+			insertions = previous_row[j + 1] + 1
+			deletions = current_row[j] + 1
+			substitutions = previous_row[j] + (c1 != c2)
+			current_row.append(min(insertions, deletions, substitutions))
+		previous_row = current_row
+		
+	return previous_row[-1]
+
+def lev(a, b):
+	if not a: return len(b)
+	if not b: return len(a)
+	return min(lev(a[1:], b[1:])+(a[0] != b[0]), lev(a[1:], b)+1, lev(a, b[1:])+1)
+
+
+
 def main():
 	show = "House"
 	title = getEpisodeName( show, 3, 11)
--- pytvrenametest.py
+++ pytvrenametest.py
@@ -15,9 +15,25 @@ class TestAgainstFile(unittest.TestCase):
 
 	def testShow(self):
 		"""tests whether the name of """
-		for line in self.file:
-			print line
+		# for line in self.file:
+		# 	print line
 		assert 2 == 2
+	
+class TestEpisodeGuide(unittest.TestCase):
+	"""docstring for TestEpisodeGuide"""
+
+	def setUp(self):
+		""" """
+		self.show = "House"
+		self.episode = 11
+		self.season = 3
+		self.title  = "Words and Deeds"
+		
+	def testGetEpisodeName(self):
+		""" """
+		assert self.title == pytvrename.getEpisodeName( self.show, self.season, self.episode)
+	
+		
 		
 	# def testSeason(self):
 	# 	"""tests whether the name of """
